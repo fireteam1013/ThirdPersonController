5455116fc61a4f3621245aa7563c59b4d015a9f8
fireteam1013 <emurabata@gmail.com>
2016-07-07 06:46:58 -0700

    send

---
+++ New Unity Project/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+    Rigidbody rb;
+    float h;
+    float v;
+    float walkSpeed = 40f;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        h = Input.GetAxisRaw("Horizontal");
+        v = Input.GetAxisRaw("Vertical");
+    }
+
+    void FixedUpdate()
+    {
+        rb.velocity = new Vector3(h * walkSpeed * Time.deltaTime, 0, v * walkSpeed * Time.deltaTime);
+    }
+}
